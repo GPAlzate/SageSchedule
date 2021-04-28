26f6adefa376432d4061fa491b1e49353038e89c
Gabriel Alzate <gabealzate@Gabriels-MacBook-Air.local>
2021-04-28 11:32:37 +0800

    changed the way fall and spring are displayed

--- app.py
+++ app.py
@@ -8,18 +8,33 @@ import re
 import json
 
 from flask import Flask, redirect, render_template, request, session
+
+SCHOOL_YEAR = 21
+NUM_SEMS = 8
+
 app = Flask(__name__)
 app.secret_key = 'all our celebrities keep dying'
 
 df = get_courses()
 
+def create_semesters():
+    semesters = {}
+    for i in range(NUM_SEMS):
+        # create a dictionary of semesters in session
+        # each sem will store the courses selected by the user
+        semesters[f"{'Spring' if i % 2 else 'Fall'} {SCHOOL_YEAR + (i + 1)//2}"] = None
+
+    # save the dictionary of semester strings
+    session['semesters'] = semesters
+
 # The home page
 @app.route('/')
 def index():
     if 'school_year' not in session:
-        session['school_year'] = 21
-    if 'semesters' not in session:
-        session['semesters'] = 8
+        session['school_year'] = SCHOOL_YEAR
+    if 'num_sems' not in session:
+        session['num_sems'] = NUM_SEMS
+        create_semesters()
     return render_template('index.html', courses=None)
 
 @app.route('/search')
